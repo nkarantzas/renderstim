fa518ac6cdbedeb722fe0620774f68f779a0da6b
Nick Karantzas <39784026+nkarantzas@users.noreply.github.com>
2023-02-27 13:43:01 -0600

    Delete renderstim/schema/.ipynb_checkpoints directory

--- renderstim/schema/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,4 +0,0 @@
-import datajoint as dj
-
-# schema = dj.schema("nikoskar_rendered_images")
-schema = dj.schema("pipeline_rendered_images")
--- renderstim/schema/.ipynb_checkpoints/scenes-checkpoint.py
+++
@@ -1,9 +0,0 @@
-from .templates.rendered_scenes import RenderedScenesBase
-from .main import LatentDataset
-from . import schema
-
-
-@schema
-class RenderedScenes(RenderedScenesBase):
-    scene_config_table = LatentDataset.SceneConfig
-
